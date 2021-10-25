9cec0d2352e12b3e46cb3847ac6b95a781f02817
p22tpearson <p22tpearson@fairfieldprep.org>
2021-10-25 10:16:15 -0400

    lab 3-1

---
+++ lab_3-1.py
@@ -0,0 +1,14 @@
+# Author TJP 10/25/21
+import math
+import time
+
+start_time = time.time_ns()
+
+print (2**3)
+print(math.pow(2,3))
+
+end_time = time.process_time()
+
+final_time = end_time-start_time
+
+print(final_time)
