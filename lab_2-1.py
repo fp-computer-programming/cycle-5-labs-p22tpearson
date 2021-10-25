d7fd6e7d5ce71769b372a7594c7ffc2c4be5d846
p22tpearson <p22tpearson@fairfieldprep.org>
2021-10-25 09:53:36 -0400

    lab 2-1

---
+++ lab_2-1.py
@@ -0,0 +1,17 @@
+# Author TJP 10/21/21
+import math
+
+# number 1
+print(math.ceil(7.15))
+
+# number 2
+print(math.floor(19.156))
+
+# number 3
+print(math.fabs(-42))
+
+# number 4
+print(math.pow(2, 3))
+
+# number 5
+print(math.log10(100))
